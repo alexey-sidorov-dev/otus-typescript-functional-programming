c1e6b659e91559ac2fb5a889a956391cb18e1ce2
Alexey Sidorov <alexey.sidorov.dev@mail.ru>
2022-05-12 00:01:52 +0300

    updated husky to v8

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,8 +3,5 @@ module.exports = {
     "eslint --config eslint.config.js --ext .js,.ts --cache --fix",
     "prettier --write",
   ],
-  "*.ts": [
-    `eslint --config eslint.config.js --fix --plugin tsc --rule 'tsc/config: [2, {configFile: "tsconfig.json"}]'`,
-  ],
   "*.{json,md,yml,html,css,scss}": "prettier --write",
 };
